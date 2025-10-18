d631038f7e36557ec8c24e45d9fa635d6c421150
Charly Gomez <charly.gomez@sentry.io>
2025-10-18 19:22:43 +0200

    better client instrumentation

--- app/sentry-instrumentations/client.ts
+++ app/sentry-instrumentations/client.ts
@@ -1,4 +1,6 @@
 import type { unstable_ClientInstrumentation } from "react-router";
+import * as Sentry from "@sentry/browser";
+import { BrowserClient } from "@sentry/browser";
 
 async function logPerformance(fn: () => Promise<void>, name: string) {
   const start = performance.now();
@@ -6,20 +8,36 @@ async function logPerformance(fn: () => Promise<void>, name: string) {
   console.log(`${name} done in ${performance.now() - start}ms`);
 }
 
+const client = Sentry.getClient() as BrowserClient;
+
 export const clientInstrumentations: unstable_ClientInstrumentation = {
   router({ instrument }) {
     instrument({
       navigate: async (fn, info) => {
-        console.log(`navigate ${info.to}`);
-        logPerformance(fn, "navigate");
+        console.log("navigate", info);
+        Sentry.startBrowserTracingNavigationSpan(client, {
+          name: `navigate ${info.to}`,
+        });
+        fn();
       },
     });
   },
   route({ instrument, id }) {
     instrument({
-      middleware: (fn) => logPerformance(fn, "middleware"),
-      loader: (fn) => logPerformance(fn, "loader"),
-      action: (fn) => logPerformance(fn, "action"),
+      middleware: (fn) =>
+        Sentry.startSpan({ name: "rr.middleware" }, async () => {
+          await fn();
+        }),
+      loader: (fn, info) =>
+        Sentry.startSpan(
+          {
+            name: `loader ${info.unstable_pattern}`,
+          },
+          async () => {
+            await fn();
+          }
+        ),
+      action: (fn) => Sentry.startSpan({ name: "rr.action" }, fn),
     });
   },
 };
